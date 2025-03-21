2cf116f5cb5fbb8dbabf63f8450e49b1d34cbd3c
popinnovationsinc <79021586+popinnovationsinc@users.noreply.github.com>
2025-03-21 06:10:22 -0500

    Set up patients and treatments elements

--- app/Models/Treatment.php
+++ app/Models/Treatment.php
@@ -5,9 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Casts\MoneyCast;
 
 class Treatment extends Model
 {
+    protected $casts = [
+        'price' => MoneyCast::class,
+    ];
+    
     public function patient(): BelongsTo
     {
         return $this->belongsTo(Patient::class);
